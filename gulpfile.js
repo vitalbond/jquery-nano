fef243a64e089abeebcf975bda69af1da475050e
Vitaly Bondaruk <vitbondaruk@gmail.com>
2020-03-03 16:18:12 +0300

    gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -2,13 +2,16 @@ const gulp = require('gulp');
 const concat = require('gulp-concat');
 const uglify = require('gulp-uglify/composer')(require('uglify-es'), console);
 
-gulp.task('watch', function() {
-	return gulp.watch(['src/*.js'], gulp.series('build'));
-});
+function watch() {
+	return gulp.watch(['src/*.js'], gulp.series(build));
+}
 
-gulp.task('build', function() {
+function build() {
 	return gulp.src(['src/core.js', 'src/*.js'], { sourcemaps: false })
 		.pipe(concat('jquery-nano.min.js'))
 		.pipe(uglify())
 		.pipe(gulp.dest('build', { sourcemaps: false }));
-});
+}
+
+module.exports.watch = watch;
+module.exports.build = build;
