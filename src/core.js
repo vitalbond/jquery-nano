2cea17e243474eb4fb372a36f0b8ac96b845726f
Vitaly Bondaruk <vitbondaruk@gmail.com>
2020-04-07 13:42:26 +0300

    fix preventDefault

--- src/core.js
+++ src/core.js
@@ -161,7 +161,11 @@ const $ = (function () {
 
 		on(eventName, ...args) {
 			this.nodes.forEach(el => {
-				el.addEventListener(eventName, typeof args[0] === 'function' ? args[0] : function (e) {
+				el.addEventListener(eventName, typeof args[0] === 'function' ? function (e) {
+					if (args[0].call(el, e) === false) {
+						e.preventDefault();
+					}
+				} : function (e) {
 					for (let target = e.target; target && target !== this; target = target.parentNode) {
 						if (target.matches(args[0])) {
 							if (args[1].call(target, e) === false) {
